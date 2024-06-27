64c59d8989c347991d976a971ce29c63b1d83400
wasswarichard <wasswarichard55@gmail.com>
2024-06-27 08:50:04 +0300

    first commit

--- solution3_mergePdf.js
+++ solution3_mergePdf.js
@@ -1,13 +1,15 @@
 (async () => {
-    console.time('merging');
     try{
+        console.time('merging');
         const {default: PDFMerger} = await import('pdf-merger-js');
         const merger =  new PDFMerger();
-        await merger.add('GHR Licence to Reside (24.25).pdf');
-        await merger.add('OL-3158086.pdf');
+        await merger.add('PDFA.pdf');
+        await merger.add('PDFB.pdf');
         await merger.save('merged.pdf');
     }catch(e){
         console.error(e);
+    }finally {
+        console.timeEnd('merging');
     }
-    console.timeEnd('merging');
+
 })()
